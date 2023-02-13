fba8a915a6c3d88d9fb3c6763bcfc4dc8b7d3b18
Kader64 <w.lichton.kader@wp.pl>
2023-02-13 19:50:38 +0100

    xD

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -1,22 +1,9 @@
-﻿using ConsoleEnhancers;
-
-var canvas = new ASCIICanvas(200, 200);
-
-System.Threading.Thread.Sleep(5000);
-
-demo();
-void demo()
+﻿class Program
 {
-    int x = 0;
-    int y = 0;
-    for (int i = 0; i < 200; i++)
+
+    static void Main(string[] args)
     {
-        canvas.fillStyle = EscapeColor.Random();
-        canvas.SetPoint(x, y);
-        System.Threading.Thread.Sleep(50);
-        x++;
-        y++;
+        
+        
     }
-    canvas.flushBuffer();
-    demo();
 }
