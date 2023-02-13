6b9c3c5839a2a007176a9a85a296517e3b82060b
Kader64 <w.lichton.kader@wp.pl>
2023-02-13 18:27:06 +0100

    + Dodanie klasy ConsoleManager

---
+++ ATM/ConsoleManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    internal class ConsoleManager
+    {
+        [DllImport("user32.dll")]
+        private static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        public ConsoleManager()
+        {
+            IntPtr handle = GetConsoleWindow();
+            IntPtr sysMenu = GetSystemMenu(handle, false);
+            if (handle != IntPtr.Zero)
+            {
+                DeleteMenu(sysMenu, 0xF030, 0x00000000);
+                DeleteMenu(sysMenu, 0xF000, 0x00000000);
+            }
+            Console.CursorVisible = false;
+        }
+        public static void setConsoleSize(int width, int height)
+        {
+            Console.SetWindowSize(width, height);
+            Console.SetBufferSize(width, height);
+        }
+    }
+}
