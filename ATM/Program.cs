6b8b1d1cd3b7d418b0db203c1ee523aba6b64492
M4trixStrike <kkowalczyk124@gmail.com>
2023-02-13 20:15:28 +0100

    + Poprawa metod ASCIICanvas

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -1,9 +1,23 @@
-﻿class Program
+﻿using ConsoleEnhancers;
+
+var canvas = new ASCIICanvas(200, 200);
+
+//System.Threading.Thread.Sleep(5000);
+
+demo();
+void demo()
 {
 
-    static void Main(string[] args)
+    for(int y = 0;y < 200; y+=10)
     {
-        
-        
+        for (int x = 0; x < 200; x+=10)
+        {
+            canvas.FillRect(x,y,10,10);
+        }
+        canvas.fillStyle = EscapeColor.Random();
     }
+    canvas.renderBuffer();
+    canvas.flushBuffer();
+    demo();
 }
+
