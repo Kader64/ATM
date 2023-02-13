27c166d0479efbb7d9e26efb82c78900f0f745ef
M4trixStrike <kkowalczyk124@gmail.com>
2023-02-13 20:40:12 +0100

    +Działa

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -7,7 +7,30 @@ class Program
 
     static void Main(string[] args)
     {
-        
+
+        ConsoleGUI.SetConsoleFont(3,3,0,0);
+        Console.SetWindowSize(200,200);
+
+        var canvas = new ASCIICanvas(100, 100);
+
+        demo();
+
+        void demo(){
+            for (int y = 0; y < 100; y += 10)
+            {
+                for (int x = 0; x < 100; x += 10)
+                {
+                    canvas.FillRect(x, y, 10, 10);
+                    canvas.fillStyle = EscapeColor.Random();
+                }
+                
+            }
+            canvas.renderBuffer();
+            System.Threading.Thread.Sleep(500);
+            demo();
+        }
         
     }
 }
+
+
