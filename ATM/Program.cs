0f6b91e522a080dcb2b3aea2b71b8f8446fa7732
M4trixStrike <115814447+M4trixStrike@users.noreply.github.com>
2023-02-14 20:17:28 +0100

    + Rozmiary

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -13,12 +13,12 @@ class Program
         ConsoleManager.SetConsoleFont(3,3,0,0);
         ConsoleManager.blockWindowResize();
         Console.CursorVisible = false;
-        Console.SetWindowSize((int)Console.LargestWindowWidth / 2, (int)Console.LargestWindowHeight / 2);
-        Console.SetBufferSize((int)Console.LargestWindowWidth / 2, (int)Console.LargestWindowHeight / 2);
+        Console.SetBufferSize(320, 180);
+        Console.SetWindowSize(322, 182);
+
         Console.SetCursorPosition(0, 0);
 
-
-        var canvas = new ASCIICanvas(Console.BufferWidth-1,100);
+        var canvas = new ASCIICanvas(Console.BufferWidth-1,Console.BufferHeight-1);
 
         int y = 90;
 
@@ -41,8 +41,8 @@ class Program
             x -= z;
             y -= f;
 
-            if (x > 90 || x <= 0) z = -z;
-            if (y > 90 || y <= 0) f = -f;
+            if (x > 310 || x <= 0) z = -z;
+            if (y > 170 || y <= 0) f = -f;
 
             canvas.renderBuffer();
             Thread.Sleep(10);
