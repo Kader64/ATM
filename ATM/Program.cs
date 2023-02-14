d48ca701e0e29cab9b08c54f697147a7b37d46b2
M4trixStrike <kkowalczyk124@gmail.com>
2023-02-14 16:15:38 +0100

    +Zmiana nazwy jednej z klas - Usunięcie starego demo

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -8,29 +8,12 @@ class Program
     static void Main(string[] args)
     {
 
-        ConsoleGUI.SetConsoleFont(3,3,0,0);
-        ConsoleGUI.blockWindowResize();
+        ConsoleManager.SetConsoleFont(3,3,0,0);
+        ConsoleManager.blockWindowResize();
         Console.SetWindowSize(200, 200);
         Console.SetBufferSize(200, 200);
 
         var canvas = new ASCIICanvas(100, 100);
-
-        //demo();
-
-        //void demo(){
-        //    for (int y = 0; y < 100; y += 10)
-        //    {
-        //        for (int x = 0; x < 100; x += 10)
-        //        {
-        //            canvas.StrokeRect(x, y, 10, 10);
-        //            canvas.strokeStyle = EscapeColor.Random();
-        //        }
-
-        //    }
-        //    canvas.renderBuffer();
-        //    Thread.Sleep(500);
-        //    demo();
-        //}
 
         int y = 90;
 
